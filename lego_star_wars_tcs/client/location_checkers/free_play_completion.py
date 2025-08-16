665edf816e01522d679fdd6bb2ef92dbd2dfdc06
Thomas Barlow <github@mysterymayhem.co.uk>
2025-08-16 22:30:00 +0100

    v0.2.2 fix for !collect-ed chapter completions

--- lego_star_wars_tcs/client/location_checkers/free_play_completion.py
+++ lego_star_wars_tcs/client/location_checkers/free_play_completion.py
@@ -102,12 +102,13 @@ class FreePlayChapterCompletionChecker:
         # does 'Save and Exit', changing the Level ID to a 'status' level and accidentally sending a location check.
         current_level_id = ctx.read_current_level_id()
         completion_location_id = STATUS_LEVEL_ID_TO_AP_ID.get(current_level_id)
+        area = STATUS_LEVEL_ID_TO_AREA.get(current_level_id)
         if (completion_location_id is not None
-                and ctx.is_location_sendable(completion_location_id)
+                and area is not None
+                and area not in self.completed_free_play
                 and is_in_free_play(ctx)
                 and is_status_level_completion(ctx)):
             self.sent_locations.add(completion_location_id)
-            area = STATUS_LEVEL_ID_TO_AREA[current_level_id]
             self.completed_free_play.add(area)
             ctx.write_byte(area.address + area.UNLOCKED_OFFSET, 0b11)
 
